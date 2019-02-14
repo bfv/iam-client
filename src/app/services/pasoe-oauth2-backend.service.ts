da95800894e632390f2bed8085d5f32f4dd7c92e
bfv <dev@bfv.io>
2019-02-14 08:19:02 +0100

    fixed Authorization header

--- src/app/services/pasoe-oauth2-backend.service.ts
+++ src/app/services/pasoe-oauth2-backend.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
@@ -18,20 +18,19 @@ export class PasoeOauth2BackendService {
       params = params.set('openedge', 'true');  // params is IMMUTABLE, so re-assign!
     }
 
+    console.log('access_token:');
+    console.log(this.auth.accessToken);
+
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', 'Bearer ' + this.auth.accessToken);
+
     const options = {
+      headers: headers,
       params: params
     };
 
-    const observable = this.http.get(this.baseUrl + '/noauth/web/get', options);
+    const observable = this.http.get(this.baseUrl + '/oauth2/web/get', options);
 
     return observable;
   }
-
-  getCustomers(includeOpenEdgeInfo = false): Observable<Object> {
-
-    const observable = this.http.get(this.baseUrl + '/oauth2/web/customer');
-
-    return observable;
-  }
-
 }
