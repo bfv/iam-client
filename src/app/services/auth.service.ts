ac1204b3a3c5c8b6ed3ce4e1c11a0398d64170aa
bfv <dev@bfv.io>
2020-03-11 11:10:13 +0100

    made auth service more generic

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,96 +6,101 @@ import { BehaviorSubject } from 'rxjs';
 import { isFulfilled } from 'q';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
 
-  readonly authServerUrl = 'http://localhost:8081';
-  readonly tokenUrl = this.authServerUrl + '/auth/realms/asref1/protocol/openid-connect/token';
-  readonly authUrl = this.authServerUrl + '/auth/realms/asref1/protocol/openid-connect/auth';
-  accessToken = '';
+    readonly clientId = 'angular-client';
+    readonly clientIdDirect = 'angular-client-direct';
+    readonly realmName = 'pasoe';
+    readonly authServerUrl = 'http://keycloak.bfv.io:8081';
+    readonly tokenUrl = this.authServerUrl + '/auth/realms/' + this.realmName + '/protocol/openid-connect/token';
+    readonly authUrl = this.authServerUrl + '/auth/realms/' + this.realmName + '/protocol/openid-connect/auth';
 
+    accessToken = '';
+    fullLoginResponse: Object;
 
-  fullLoginResponse: Object;
+    private loggedInSource = new BehaviorSubject<boolean>(false);
+    loggedIn = this.loggedInSource.asObservable();
 
-  private loggedInSource = new BehaviorSubject<boolean>(false);
-  loggedIn = this.loggedInSource.asObservable();
+    private implicitAccessTokenSource = new BehaviorSubject<string>('');
+    implicitAccessToken = this.implicitAccessTokenSource.asObservable();
 
-  private implicitAccessTokenSource = new BehaviorSubject<string>('');
-  implicitAccessToken = this.implicitAccessTokenSource.asObservable();
+    constructor(private http: HttpClient, private keycloak: KeycloakService) {
 
-  constructor(private http: HttpClient, private keycloak: KeycloakService) {
+        this.keycloak.init(
+            {
+                config: {
+                    clientId: this.clientId,
+                    realm: this.realmName,
+                    url: this.authServerUrl + '/auth'
+                },
+                initOptions: {
+                    flow: 'implicit'
+                },
+                bearerExcludedUrls: [
+                    'http://localhost:15900/noauth',
+                    'http://localhost:15900/oauth2/web/get',
+                    this.tokenUrl
+                ]
+            }
+        ).then(() => {
 
-    this.keycloak.init(
-      {
-        config: {
-          clientId: 'angular-test-client',
-          realm: 'asref1',
-          url: 'http://keycloak.bfv.io:8081/auth'
-        },
-        initOptions: {
-          flow: 'implicit'
-        },
-        bearerExcludedUrls: [
-          'http://localhost:15900/noauth',
-          'http://localhost:15900/oauth2/web/get',
-          'http://localhost:8081/auth/realms/asref1/protocol/openid-connect/token'
-        ]
-      }).then(() => {
-
-        this.keycloak.isLoggedIn().then(loggedIn => {
-          this.setLoggedIn(loggedIn);
-          if (loggedIn) {
-            this.keycloak.getToken().then(token => {
-              this.implicitAccessTokenSource.next(token);
+            this.keycloak.isLoggedIn().then(loggedIn => {
+                this.setLoggedIn(loggedIn);
+                if (loggedIn) {
+                    this.keycloak.getToken().then(token => {
+                        this.implicitAccessTokenSource.next(token);
+                    });
+                }
             });
-          }
-        });
-      });
-  }
-
-  login(userid: string, password: string): Promise<boolean> {
-
-    console.log('login attempt for:', userid);
-
-    const promise = new Promise<boolean>((resolve, reject) => {
-      this.http.post(
-        this.tokenUrl,
-        `grant_type=password&username=${userid}&password=${password}&client_id=asref1-test-client&scope=`,
-        {
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          }
         }
-      ).subscribe(data => {
+        );
+    }
 
-        this.accessToken = data['access_token'];
-        this.fullLoginResponse = data;
+    login(userid: string, password: string): Promise<boolean> {
 
-        resolve(true);
-      }, error => {
-        reject(error);
-      });
-    });
+        console.log('login attempt for:', userid);
 
-    return promise;
+        const promise = new Promise<boolean>((resolve, reject) => {
+            this.http.post(
+                this.tokenUrl,
+                `grant_type=password&username=${userid}&password=${password}&client_id=` + this.clientIdDirect + `&scope=`,
+                {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    }
+                }
+            ).subscribe(data => {
 
-  }
+                this.accessToken = data['access_token'];
+                this.fullLoginResponse = data;
 
-  logout() {
-    this.accessToken = '';
-    this.fullLoginResponse = null;
-  }
+                resolve(true);
+            }, error => {
+                reject(error);
+            });
+        });
 
-  implicitLogin(): void {
-    const promise = this.keycloak.login();
-  }
+        return promise;
 
-  implicitLogout() {
-    this.keycloak.logout();
-  }
+    }
 
-  private setLoggedIn(loggedIn: boolean) {
-    this.loggedInSource.next(loggedIn);
-  }
+    logout() {
+        this.accessToken = '';
+        this.fullLoginResponse = null;
+    }
+
+    implicitLogin(): void {
+        const promise = this.keycloak.login();
+    }
+
+    implicitLogout() {
+        this.keycloak.logout();
+    }
+
+    private setLoggedIn(loggedIn: boolean) {
+        this.loggedInSource.next(loggedIn);
+    }
+
 }
